d4cf61caba9a2587658dc0028e662ae4d4a30d4c
Yannick Meeus <yannick.meeus@gmail.com>
2021-07-09 11:05:44 +0300

    Add diesel (ORM) and set up migrations

--- backend/migrations/2021-07-07-162755_create_shops/up.sql
+++ backend/migrations/2021-07-07-162755_create_shops/up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE shops (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    key TEXT NOT NULL DEFAULT gen_random_uuid(),
+    key UUID NOT NULL DEFAULT gen_random_uuid(),
     name TEXT NOT NULL
 );
 
 CREATE UNIQUE INDEX ix_shops_key on shops (key);
+CREATE UNIQUE INDEX ix_shops_name on shops (name);
