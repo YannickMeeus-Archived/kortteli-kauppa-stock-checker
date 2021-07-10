f6627df26a85bbd4c63d85dea0f676d3b06f32ad
Yannick Meeus <yannick.meeus@gmail.com>
2021-07-10 12:43:20 +0300

    Address clippy (rust linter) violations

--- backend/src/infrastructure/version.rs
+++ backend/src/infrastructure/version.rs
@@ -2,6 +2,6 @@ use rocket::{serde::json::{Value, json}};
 
 #[get("/_version")]
 pub fn version() -> Value {
-    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
     json!({"data": VERSION})
 }
