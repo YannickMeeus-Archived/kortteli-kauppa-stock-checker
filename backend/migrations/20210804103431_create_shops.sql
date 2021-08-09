fc6412326a9a9bf0c0515a569755c1a28bd1c965
Yannick Meeus <yannick.meeus@gmail.com>
2021-08-09 15:30:28 +0300

    First Kermit on Rails Project move

--- backend/migrations/20210804103431_create_shops.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE shops
-(
-    id   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    key  UUID NOT NULL DEFAULT gen_random_uuid(),
-    name TEXT NOT NULL
-);
-
-CREATE UNIQUE INDEX ix_shops_key ON shops (key);
-CREATE UNIQUE INDEX ix_shops_name ON shops (name);
--- backend/src/infrastructure/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod ping;
-pub mod version;
-
-pub use self::ping::ping as ping_handler;
-pub use self::version::version as version_handler;
