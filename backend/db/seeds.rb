71ff69e4ea9982af1571538c671e1281d70b41c9
Yannick Meeus <yannick.meeus@gmail.com>
2021-08-16 12:59:12 +0300

    Fix up some db issues and seed database

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+shop_names = %w[Kuninkaantammi Katajanokka Olari Painiitty Talinranta Keimolanmaki]
+shop_names.each { |shop_name| Shop.find_or_create_by(name: shop_name)}
+
