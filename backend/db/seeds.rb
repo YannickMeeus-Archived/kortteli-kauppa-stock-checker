3ba1e0b8904fd055e54eb1c6eea6c69565c969a4
Yannick Meeus <yannick.meeus@gmail.com>
2021-08-29 20:24:27 +0300

    Allow seed to fail if db doesn't exist

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,10 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-shop_names = %w[Kuninkaantammi Katajanokka Olari Painiitty Talinranta Keimolanmaki]
-shop_names.each { |shop_name| Shop.find_or_create_by(name: shop_name)}
+begin
+  shop_names = %w[Kuninkaantammi Katajanokka Olari Painiitty Talinranta Keimolanmaki]
+  shop_names.each { |shop_name| Shop.find_or_create_by(name: shop_name)}
+rescue
+  # Ignored
+end
+
 
