5ec940ce1aea78b68e5fdbf5d078720f76a5611d
Yannick Meeus <yannick.meeus@gmail.com>
2021-09-01 10:08:59 +0300

    Enable downsync in production in async mode - no separate nodes required

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.active_job.queue_adapter = :good_job
+  config.good_job.execution_mode = :async
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
