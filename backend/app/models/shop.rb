40b7f3eed7f99fbc038494a0dac2cfca6ba0cf58
Yannick Meeus <yannick.meeus@gmail.com>
2021-10-06 19:26:03 +0300

    Run annotate --models && annotate --routes

--- backend/app/models/shop.rb
+++ backend/app/models/shop.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: shops
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_shops_on_name  (name) UNIQUE
+#
 class Shop < ApplicationRecord
 end
--- backend/spec/factories/ingested_product_informations.rb
+++ backend/spec/factories/ingested_product_informations.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: ingested_product_informations
+#
+#  id         :uuid             not null, primary key
+#  shop       :uuid
+#  raw_data   :jsonb
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :ingested_product_information do
     shop { '' }
