b3f7d5088ce529f3bdf470c12550092913784890
Yannick Meeus <yannick.meeus@gmail.com>
2021-11-08 10:03:58 +0200

    Add secret ping route to test authenticated routes

--- backend_node/__tests__/http-server/ping.e2e.test.ts
+++ backend_node/__tests__/http-server/ping.e2e.test.ts
@@ -1,5 +1,5 @@
 import Request from "supertest";
-import { makeFakeHttpApi } from "./makeTestingApi";
+import { makeFakeHttpApi, testingApiKey } from "./makeTestingApi";
 
 describe("Ping Route", () => {
   it("should return a pong", async () => {
@@ -7,5 +7,21 @@ describe("Ping Route", () => {
 
     expect(statusCode).toEqual(200);
     expect(body).toEqual({ data: "pong" });
+  });
+});
+
+describe("Secure Ping Route", () => {
+  it("When the correct api key is provided, it should respond with a secret pong", async () => {
+    const { statusCode, body } = await Request(makeFakeHttpApi())
+      .get("/_secure_ping")
+      .set("x-api-key", testingApiKey);
+    expect(statusCode).toEqual(200);
+    expect(body).toEqual({ data: "secret pong" });
+  });
+  it("When no key is provided, it should disallow access", async () => {
+    const { statusCode } = await Request(makeFakeHttpApi()).get(
+      "/_secure_ping"
+    );
+    expect(statusCode).toEqual(401);
   });
 });
