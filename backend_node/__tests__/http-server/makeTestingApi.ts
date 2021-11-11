43356ee4c5c3721f2c5480a2df80d91410193cdc
Yannick Meeus <yannick.meeus@gmail.com>
2021-11-11 15:35:40 +0200

    Fix up test http instantiation

--- backend_node/__tests__/http-server/makeTestingApi.ts
+++ backend_node/__tests__/http-server/makeTestingApi.ts
@@ -1,5 +1,6 @@
 import { makeHttpApi } from "../../src/http-server/composition-root";
 
 const testingApiKey = "testing-api-key";
-const makeFakeHttpApi = () => makeHttpApi({ apiKey: testingApiKey });
+const makeFakeHttpApi = () =>
+  makeHttpApi({ security: { apiKey: testingApiKey } });
 export { testingApiKey, makeFakeHttpApi };
