619324a9a95c11748d11495d32f51bec4585935b
Yannick Meeus <yannick.meeus@gmail.com>
2021-11-17 12:50:45 +0200

    Add raw_inventory_data table

---
+++ backend_node/migrations/1637145998094_create-raw-inventory-data.js
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.sql(`
+  CREATE TABLE raw_inventory_data
+(
+    id         uuid DEFAULT gen_random_uuid() NOT NULL CONSTRAINT ingested_product_informations_pkey  PRIMARY KEY,
+    shop       uuid,
+    raw_data   jsonb,
+    created_at TIMESTAMP(6)                   NOT NULL,
+    updated_at TIMESTAMP(6)                   NOT NULL
+);
+  `);
+  pgm.sql(`
+CREATE TRIGGER set_updated_on_raw_inventory_data
+  BEFORE
+      UPDATE ON raw_inventory_data
+  FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at_timestamp();
+  `)
+};
+
+exports.down = pgm => {
+  pgm.sql(`DROP TRIGGER set_updated_on_raw_inventory_data ON raw_inventory_data;`)
+  pgm.sql(`
+    DROP TABLE IF EXISTS raw_inventory_data;
+  `)
+};
