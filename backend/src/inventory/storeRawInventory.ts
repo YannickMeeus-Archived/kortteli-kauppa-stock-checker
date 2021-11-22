475fd38c19a0d5fd3d3506c735a9cb08975ac767
Yannick Meeus <yannick.meeus@gmail.com>
2021-11-22 15:42:02 +0200

    Create simple interface and in memory imp for storing raw inventory files

---
+++ backend/src/inventory/storeRawInventory.ts
@@ -0,0 +1,14 @@
+import { ShopId } from "../shops";
+import { CabinetItem } from "./models/cabinetItem";
+
+interface StoreRawInventory {
+  forShop(id: ShopId, rawInventory: CabinetItem[]): Promise<void>;
+}
+
+class StoreRawInventoryInMemory implements StoreRawInventory {
+  constructor(private readonly storeInventories: Map<ShopId, CabinetItem[]>) {}
+  async forShop(id: ShopId, rawInventory: CabinetItem[]): Promise<void> {
+    this.storeInventories.set(id, rawInventory);
+  }
+}
+export { StoreRawInventory, StoreRawInventoryInMemory };
