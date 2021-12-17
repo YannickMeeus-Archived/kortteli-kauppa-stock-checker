99547b6f7ba7af156e9291d043175942aa323618
Yannick Meeus <yannick.meeus@gmail.com>
2021-12-17 10:05:22 +0200

    Add snapshot archiving command

--- backend/src/domain/inventory/archiveSnapshot.ts
+++ backend/src/domain/inventory/archiveSnapshot.ts
@@ -2,21 +2,19 @@ import { SnapshotNotFoundError } from "./models/errors/snapshotNotFoundError";
 import { Snapshot, SnapshotId } from "./models/snapshots/snapshot";
 
 interface ArchiveSnapshot {
-  execute(id: SnapshotId): Promise<Snapshot>;
+  execute(id: SnapshotId): Promise<void>;
 }
 
 class ArchiveSnapshotInMemory implements ArchiveSnapshot {
   constructor(private readonly snapShots: Map<string, Snapshot>) {}
 
-  async execute(id: SnapshotId): Promise<Snapshot> {
+  async execute(id: SnapshotId): Promise<void> {
     const foundSnapshot = this.snapShots.get(id);
     if (!foundSnapshot) {
       throw new SnapshotNotFoundError(id);
     }
 
     this.snapShots.set(id, { ...foundSnapshot, archived: true });
-
-    return foundSnapshot;
   }
 }
 
